0a272c2958f005f4d6e66f1cd1abd8608a8d420e
hoodielive <hoodielive@gmail.com>
2018-11-13 19:27:58 -0500

    Express is fun..

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
+
 const express = require('express'); 
 const app = express(); 
+const data = require('./data/data');
 
+
+/** Default route for index **/ 
 app.get('/', (req, res) => {
-  res.send('Howdy') 
+  res.send(data);
 }); 
 
-app.get('/yo', (req, res) => {
-  res.send('Whats gup homey?'); 
+app.post('/post', (req, res) => {
+  res.send('a post request for new item')
 }); 
 
+app.put('/put', (req, res) => {
+  res.send('a put request for new item')
+}); 
+
+app.delete('/del', (req, res) => {
+  res.send('a delete request for new item')
+}); 
+
+/** Server stuff. **/ 
 const port = process.env.PORT || 3000; 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  console.log(data);
+}); 
