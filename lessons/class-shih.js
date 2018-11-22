55983fc72ee4c299182b51063cf0d8c9e32cd609
hoodielive <hoodielive@gmail.com>
2018-11-22 15:24:37 -0500

    Class of class

---
+++ lessons/class-shih.js
@@ -0,0 +1,26 @@
+class baseModel {
+    constructor(options = {}, data = []) {
+        this.name = 'Base';
+        this.url = 'http://local.dev';
+        this.data = data;
+        this.options = options; 
+    }
+    getName() {
+        console.log(`Class name: ${this.name}`);
+    }
+}
+
+class AccountModel extends baseModel {
+    constructor(options, data) {
+        super({ private: true }, ['32113123123', '524214691'])
+        this.name = 'Account Model';
+        this.url  +='/accounts/'; 
+    }
+    get accountsData() {
+        return this.data; 
+    }
+}
+
+let accounts = new AccountModel(5);
+accounts.getName();
+console.log('Data is %s', accounts.accountsData);
