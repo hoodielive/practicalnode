1c45673402578a8c0ad414beb7e194e47dd45cd9
hoodielive <hoodielive@gmail.com>
2018-11-24 22:53:18 -0500

    modules exports imports

---
+++ lessons/modules-export-import.js
@@ -0,0 +1,12 @@
+const messages = {
+  find: function(req, res, next) {
+    // 
+  }
+  add: function(req, res, next) {
+
+  }
+  format: 'title | date | author '
+  
+}
+
+exports.messages = messages; 
