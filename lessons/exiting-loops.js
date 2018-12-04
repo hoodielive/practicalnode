55038c0f2c9c170aec1816fc1b762740997e8e03
hoodielive <hoodielive@gmail.com>
2018-12-04 17:49:52 -0500

    sometimes we want to exit a loop before it has finished

--- lessons/exiting-loops.js
+++ lessons/exiting-loops.js
@@ -1,3 +1,5 @@
+// exit a loop before it has finished with the break keyword
+
 for (let i = 0; i < 5; i++) {
   if(Math.random() > 0.5) {
     console.log("Breaking out of the loop when i is " + i);  
