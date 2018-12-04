c60e88cd2a29543ada9e4f60b452bdc37edcf991
hoodielive <hoodielive@gmail.com>
2018-12-04 18:03:29 -0500

    use the split method to convert string to array

--- lessons/use-split-to-convert-string-to-array.js
+++ lessons/use-split-to-convert-string-to-array.js
@@ -4,3 +4,10 @@ let string = "hello aeonos";
 
 console.log(string.split(""));
 console.log(string.split(" "));
+
+// if you pass a delimiter into the split method, the delimiting values will be removed from the array 
+let dashedString = "lots-of-dashes-here"; 
+let removedDashes = dashedString.split("-"); 
+console.log(removedDashes);
+
+
