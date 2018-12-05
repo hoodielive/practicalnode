57c5db919646936050282c63f6f1334dfd75082c
hoodielive <hoodielive@gmail.com>
2018-12-05 18:46:46 -0500

    function - scope

---
+++ lessons/function-lessons/function-scope.js
@@ -0,0 +1,23 @@
+// make a function for add, subtract, multiply, and divide - each of these functions should accept 2 params 
+// and return the sum, difference, product and quotient 
+
+function add(a, b) {
+  return a + b; 
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b; 
+}
+
+function divide(a, b) {
+  return a / b; 
+}
+
+add(2, 2); 
+subtract(2,2); 
+multiply(2,2);
+divide(2,2);
